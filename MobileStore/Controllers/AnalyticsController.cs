880d32102aaedda82cd2f7f5926529a7b4c5aa18
labib <labib@SWD-LLBIB>
2017-09-08 11:52:40 +0600

    analytics added

---
+++ MobileStore/Controllers/AnalyticsController.cs
@@ -0,0 +1,26 @@
+﻿using MobileStore.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MobileStore.Controllers
+{
+    [RoutePrefix("api/Analytics")]
+    public class AnalyticsController : ApiController
+    {
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        [HttpGet]
+        [ActionName("getTop3Products")]
+        public IHttpActionResult getTop3Products()
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
